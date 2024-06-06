6bbaade933152a19e75c3c76254c788c3d0cb8b1
ACER <ACER@DESKTOP-BH5G8C0>
2024-06-06 09:52:55 -0600

    Agregar archivos de proyecto.

---
+++ ExamenFinal/Laptop.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata;
+
+namespace ExamenFinal
+{
+    public class Laptop:Dispositivos
+    {
+        public int MemoriaRam {  get; set; }
+        public string Almacenamiento {  get; set; }
+        public Laptop()
+        {
+            MemoriaRam = 0;
+            Almacenamiento = string.Empty;
+        }
+    }
+}
